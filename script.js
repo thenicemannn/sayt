bdebb56e19bdad640a66ad39965658ad60e00902
thenicemannn <trubaua77@gmail.com>
2025-01-19 18:26:30 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -52,28 +52,43 @@ function searchProducts(event) {
     event.preventDefault(); // Забороняє перезавантаження сторінки при відправці форми
 
     let query = document.querySelector('.form_input').value.toLowerCase();
+
     let productsList = document.querySelector('.catalog');
+
     productsList.innerHTML = ''; // Очищаємо список товарів
 
     // Відображаємо товари на сторінці
     getProducts().then(function (products) {
         let productsList = document.querySelector('.catalog');
         products.forEach(function (product) {
-            if (product.title.toLowerCase().includes(query) || product.description.toLowerCase().includes(query)) {
+            let name = product.title.toLowerCase()
+            console.log(name)
+            if (name.includes(query)) {
                 productsList.innerHTML += getCardHTML(product);
             }
         });
 
         // Отримуємо всі кнопки "Купити" на сторінці
-        let buyButtons = document.querySelectorAll('.buy');
+        // let buyButtons = document.querySelectorAll('.buy');
         // Додаємо обробник подій на кожну кнопку "Купити"
-        if (buyButtons) {
-            buyButtons.forEach(function (button) {
-                button.addEventListener('click', addToCart);
-            });
-        }
+        // if (buyButtons) {
+        //     buyButtons.forEach(function (button) {
+        //         button.addEventListener('click', addToCart);
+        //     });
+        // }
     });
 }
-let searchForm = document.getElementById('searchForm')
+let searchForm = document.querySelector('.form_btn')
 console.log(searchForm)
-searchForm.addEventListener('sumbit',searchProducts);
+searchForm.addEventListener('click',searchProducts);
+
+
+let form_btn = document.querySelector('.search_link')
+let form_bts = document.querySelector('.form')
+let form_text = document.querySelector('.ban_text')
+
+form_btn.addEventListener('click', function(){
+    event.preventDefault(); 
+    form_text.classList.toggle('dn')
+    form_bts.classList.toggle('dn')
+})
